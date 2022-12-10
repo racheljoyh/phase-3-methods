551517aefa410abdc01e5d4f6bbf5a85b8a5de81
Rachel <racheljhumes@gmail.com>
2022-12-10 12:18:44 -0700

    commit tree f7ca5b2f07f4966e1eaf322fc65f36b9afe6835a

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+  print "Hello, programmer!"
+end
+
+
+def greet name
+  print "Hello, #{name}!"
+end
+
+
+
+def greet_with_default name = "programmer"
+  print "Hello, #{name}!"
+end
+
+
+def add num1,num2
+  return num1 + num2
+end
+
+
+def halve number
+  return nil unless number.class == Integer
+
+  return number / 2
+end
+
+
